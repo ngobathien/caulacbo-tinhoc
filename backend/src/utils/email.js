5b0493a1f8c3b44f5ce3a5ae3a533b3cbe9556fa
ngobathien <bathienba1@gmail.com>
2025-10-17 13:39:11 +0700

    edit file email.js

--- backend/src/utils/email.js
+++ backend/src/utils/email.js
@@ -3,6 +3,9 @@ import nodemailer from "nodemailer";
 export const sendVerificationEmail = async (email, token) => {
   const transporter = nodemailer.createTransport({
     service: "gmail", // Hoặc dịch vụ email bạn sử dụng
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false, // Sử dụng TLS (true nếu dùng port 465)
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
